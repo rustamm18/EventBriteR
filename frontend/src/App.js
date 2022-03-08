fedc441c25ae3b00da87c47022ff64946319c3ac
rustamm18 <98675965+rustamm18@users.noreply.github.com>
2022-03-07 20:49:25 -0800

    frontend/header/footer/bootswatch/bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
+import {Container} from 'react-bootstrap';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <>
-    <h1>Welcome to My EventBrite</h1>
-    <h2>Eventbrite is a ticketing and event technology platform
-    that helps businesses organize and sell 
-    tickets to events online</h2>
+    <Header />
+    <main>
+      <Container>
+      <h1>Welcome to My EventBrite</h1>
+        <h6>EventBrite is a ticketing and event technology platform
+        that helps businesses organize and sell 
+        tickets to events online</h6>
+        </Container>
+    </main>
+    <Footer /> 
     </>
    
   );
