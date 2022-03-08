a89505fe9a47af91e6cbb72da09f9482dcd43fff
rustamm18 <98675965+rustamm18@users.noreply.github.com>
2022-03-07 19:30:36 -0800

    React project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+    <h1>Welcome to My EventBrite</h1>
+    <h2>Eventbrite is a ticketing and event technology platform
+    that helps businesses organize and sell 
+    tickets to events online</h2>
+    </>
+   
+  );
+}
+
+export default App;
