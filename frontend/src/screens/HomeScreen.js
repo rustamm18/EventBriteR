0ed6524e381e20ebc5f3a3229a0cab998b410308
rustamm18 <98675965+rustamm18@users.noreply.github.com>
2022-03-27 13:54:17 -0700

    Event list reducer and action

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector} from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Event from '../components/Event'
+import { listEvents } from '../actions/eventActions'
 
 
 const HomeScreen = () => {
-  const [events, setEvents] = useState([])
+  const dispatch = useDispatch()
+  const eventList = useSelector((state) => state.
+  eventList)
+  const {loading, error, events} = eventList
   useEffect(() => {
-  const fetchEvents = async () => {
-  const {data} = await axios.get('/api/events/')
-  setEvents(data)
-  }
-
-  fetchEvents()
-}, [])
+    dispatch(listEvents())
+  }, [dispatch])
 
     
   return (
   <>
     <h1>Latest Events</h1>
-    <Row>
-      {events.map((variablenameshittyconfusingbutmeansadesireditem) => (
-        <Col key={variablenameshittyconfusingbutmeansadesireditem._id} sm={12} md={6} lg={4} xl={3}>
-          <Event product={variablenameshittyconfusingbutmeansadesireditem} />
-          </Col>
-      ))}
-    </Row>
+    {loading ? (
+      <h2>Loading...</h2>
+    ) : error ? (
+      <h3>{error}</h3>
+    ) : (<Row>
+    {events.map((variablenameshittyconfusingbutmeansadesireditem) => (
+      <Col key={variablenameshittyconfusingbutmeansadesireditem._id} sm={12} md={6} lg={4} xl={3}>
+        <Event product={variablenameshittyconfusingbutmeansadesireditem} />
+        </Col>
+    ))}
+  </Row>)}
+    
     </>
   )
 }
