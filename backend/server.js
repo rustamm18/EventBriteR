19f0e6d28cf30381fc8caab8cab08b3cd245617b
rustamm18 <98675965+rustamm18@users.noreply.github.com>
2022-03-26 22:58:08 -0700

    error handling and up to redux things uploaded

--- backend/server.js
+++ backend/server.js
@@ -16,5 +16,5 @@ app.use('/api/events', eventRoutes )
 app.use(errorHandler)
 app.use(notFound)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 7000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
