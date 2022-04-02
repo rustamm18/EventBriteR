f68a0ec3da0e6b2be9dabf7c29e258c2cf9e3853
rustamm18 <98675965+rustamm18@users.noreply.github.com>
2022-04-02 14:34:42 -0700

    cart funtionality

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,15 +1,17 @@
-import React, { useEffect} from 'react'
+import React, { useEffect, useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link, useParams } from 'react-router-dom'
-import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
+import { Link, useParams, useNavigate } from 'react-router-dom'
+import {Row, Col, Image, ListGroup, Card, Button, Form} from 'react-bootstrap'
 import Rating from '../components/Rating';
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { listEventDetails } from '../actions/eventActions'
 
 const ProductScreen = () => {
+  const [qty, setQty] = useState(0);
   const params = useParams();
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   const eventDetails = useSelector((state) => state.eventDetails)
   const { loading, error, event } = eventDetails
 
@@ -17,7 +19,9 @@ const ProductScreen = () => {
     dispatch(listEventDetails(params.id))
   }, [dispatch, params])
 
-
+    const addToCartHandler = () => {
+      navigate(`/cart/${params.id}?qty=${qty}`)
+    }
     return (
         <>
         <Link className='btn btn-light my-3' to='/'>
@@ -61,10 +65,36 @@ const ProductScreen = () => {
                   </Col>
                 </Row>
               </ListGroup.Item>
+
+              {event.countInStock > 0 && (
+                <ListGroup.Item>
+                  <Row>
+                   <Col>Qty</Col> 
+                   <Col>
+                   <Form.Control
+                    as='select'
+                    value={qty}
+                    onChange={(e) => setQty(e.target.
+                      value)}
+                   >
+                     {[...Array(event.countInStock).
+                     keys()].map((x) => (
+                       <option key={x + 1} value={x + 1}>{x + 1}</option>
+                     ))
+
+
+                     }
+                   </Form.Control>
+                   </Col>
+                  </Row>
+                </ListGroup.Item>
+              )}
+
               <ListGroup.Item>
                 <Button
                   className='btn-block'
                   type='button'
+                  onClick={addToCartHandler}
                   disabled={event.countInStock === 0}
                 >
                   Add To Cart
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,21 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import { eventListReducer, eventDetailsReducer, } from './reducers/eventReducers'
+import { cartReducer } from './reducers/cartReducers'
 import {composeWithDevTools} from 'redux-devtools-extension'
 
 const reducer = combineReducers({
    eventList: eventListReducer,
-   eventDetails: eventDetailsReducer, 
+   eventDetails: eventDetailsReducer,
+   cart: cartReducer, 
 })
-const initialState = {}
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? 
+ JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+   cart: {cartItems: cartItemsFromStorage}
+}
 const middleware = [thunk]
 const store = createStore(
                 reducer,
@@ -15,4 +23,7 @@ const store = createStore(
                 composeWithDevTools(applyMiddleware(...middleware))
                 )
 
+
+              
+ 
 export default store
